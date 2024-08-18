5681a99f6d5d4dc9c6c8ebe63f41c42d8a7c1b0c
kesla chime <keslachime.n@gmail.com>
2024-08-18 14:31:12 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 
 
